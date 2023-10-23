47ceea23ec11cfcac613c47c57d759778fe898e5
Sadiq <sadiq.said@student.moringaschool.com>
2023-10-23 21:51:46 +0300

    First commit

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        if not isinstance(value, str):
+            raise ValueError("The value must be a string.")
+        self.value = value
+
+    def is_sentence(self):
+        if self.value and self.value[-1] == '.':
+            return True
+        return False
+
+    def is_question(self):
+        if self.value and self.value[-1] == '?':
+            return True
+        return False
+
+    def is_exclamation(self):
+        if self.value and self.value[-1] == '!':
+            return True
+        return False
+
+    def count_sentences(self):
+        sentences = re.split(r'[.!?]', self.value)
+        sentences = [sentence.strip() for sentence in sentences]
+        sentences = list(filter(None, sentences))
+        return len(sentences)
